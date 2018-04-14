c6832952fa73df4b1f3923e0ca674a6073a1ea9a
lr5420511 <783621006@qq.com>
2018-04-14 14:31:50 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # promiseify
 异步流程改善，把包含callback的异步函数promise化。对于访问数据库、io操作等非常有用。
+
+对于未遵守nodejs回调函数规范（callback总是参数的最后一个，err总是callback参数的第一个）的异步函数,仅仅通过简单地调用时配置就可以了。
---
+++ demo.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const { promiseify } = require('./lib/promiseify');
+const { readFile } = require('fs');
+
+const readFilePromise = promiseify(readFile);
+
+(async(path) => {
+    const ctn1 = await readFilePromise(path, 'utf8');
+    console.log(ctn1);
+    let ctn2;
+    try {
+        ctn2 = await readFilePromise(path + '1', 'utf8');
+        console.log([ctn2, ctn1]);
+    } catch (err) {
+        console.log(err.message);
+    }
+})('./test')
+
+console.log('The first sync print.');
+
+const test = {
+    name: 'test name',
+    asyncMethod: function(callback, timeout) {
+        const cur = this;
+        setTimeout(() => {
+            if (timeout > 100)
+                return callback(
+                    null,
+                    new RangeError('timeout is too large.')
+                );
+            callback(cur, null);
+        }, timeout);
+    }
+};
+
+const testAsyncPromise = promiseify(
+    test.asyncMethod, 0, 1
+).bind(test);
+
+(async() => {
+    let obj = await testAsyncPromise(0);
+    console.log(obj.name);
+    obj.name = 'change name';
+    obj = await testAsyncPromise(100);
+    console.log(obj.name);
+    try {
+        obj = await testAsyncPromise(3000);
+        console.log(obj.name);
+    } catch (err) {
+        console.log(err.message);
+    }
+    console.log('demo end');
+})()
+
+console.log('The second sync print.');
